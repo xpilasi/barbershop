b01ed2e373438b3806a7b0799f87a134402cf935
Xavier Pilasi <pilasiha@gmail.com>
2024-04-27 22:40:35 +0200

    mobile

--- src/components/MenuTop.vue
+++ src/components/MenuTop.vue
@@ -6,6 +6,11 @@ import moustacheSvgLogo from '../assets/img/icons/moustacheSvg.svg';
 
 const menuClasses = ref('p-10 text-sm ');
 
+const scrollPosition = null;
+const mobile = true;
+const mobileNav = null;
+const windowWidth = null;
+
 //menu scrolling down
 const handleScroll = ()=>{
     if (window.scrollY > 0) {
@@ -16,7 +21,6 @@ const handleScroll = ()=>{
 
 onMounted(()=>{
     window.addEventListener('scroll',handleScroll);
-    
 });
 }
  
@@ -25,11 +29,13 @@ onMounted(()=>{
    <header>
     <nav>
 
-      <div class=' bg-barber-bg fixed top-0 w-full z-10 '>
-    <div class="lg:hidden flex justify-end mt-5">
+    <div class=' bg-barber-bg fixed top-0 w-full z-10 '>
+    <div class="flex justify-end mt-5">
 
-      <!-- Logo burber Menu -->
-      <button @click="toggleMenu" class="text-white focus:outline-none mr-10">
+      
+    </div>
+    <div>
+      <button @click="toggleMenu" v-show="mobile" class="text-white focus:outline-none mr-10">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6"
@@ -45,17 +51,24 @@ onMounted(()=>{
           />
         </svg>
       </button>
-      <!-- End logo burger Menu -->
-    </div>
-    <div :class=menuResponsive>
-        <ul class='lg:flex bg-slate-600 mx-20 text-white font-hilborn tracking-wider justify-center items-center'>
+        <ul v-show="!mobile" class=' bg-red-950 mx-20 text-white font-hilborn tracking-wider justify-center items-center'>
             <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
-            <li :class= test><a href=# :class=hoverMenu>PRECIOS</a></li>
-            <li :class=menuClasses><a href=# :class=hoverMenu>AGENDAR</a></li>
-            <li :class=menuClasses><a href=# :class=hoverMenu>UBICACIÓN</a></li>
-            <li :class=menuClasses><a href=# :class=hoverMenu>CLIENTES</a></li>
+            <li ><a href=# >PRECIOS</a></li>
+            <li ><a href=# >AGENDAR</a></li>
+            <li ><a href=# >UBICACIÓN</a></li>
+            <li ><a href=# >CLIENTES</a></li>
             <!-- <li class='sm:hidden md:hidden'><img :src=moustacheSvgLogo alt="" width="60"></li> -->
+        </ul>
+        <!-- Logo burber Menu -->
       
+      <!-- End logo burger Menu -->
+        <ul v-show="mobile" class='lg:flex bg-slate-600 mx-20 text-white font-hilborn tracking-wider justify-center items-center'>
+            <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
+            <li ><a href=# >PRECIOS</a></li>
+            <li ><a href=# >AGENDAR</a></li>
+            <li ><a href=# >UBICACIÓN</a></li>
+            <li ><a href=# >CLIENTES</a></li>
+            <!-- <li class='sm:hidden md:hidden'><img :src=moustacheSvgLogo alt="" width="60"></li> -->
         </ul>
     </div>
    </div>
