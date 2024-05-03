88b9740ace878bc71e1d5551c23710431e36cb28
Xavier Pilasi <pilasiha@gmail.com>
2024-05-04 00:03:06 +0200

    menu finally fixed

--- src/components/MenuTop.vue
+++ src/components/MenuTop.vue
@@ -69,7 +69,7 @@ onMounted(()=>{
       
       <!-- Nav no Mobile -->
       
-        <ul v-show= !mobile class='flex bg-barber-bg  text-white font-hilborn tracking-wider justify-center items-center'>
+        <ul v-show= !mobile class='flex fixed right-0 left-0  bg-barber-bg  text-white font-hilborn tracking-wider justify-center items-center'>
             <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
             <li :class=menuClasses><a href=# >PRECIOS</a></li>
             <li :class=menuClasses><a href=# >AGENDAR</a></li>
@@ -81,9 +81,10 @@ onMounted(()=>{
       
         
       <!-- Nav mobile -->
-      <div v-show=mobile class='bg-red-300 h-20'>
-
-        <button @click=toggleMenu v-show=showMenuIcon class="md:hidden  flex absolute right-0 text-white focus:outline-none h-20 w-20 justify-center items-center">
+      
+<div class='flex fixed w-full left-0 right-0 bg-barber-bg justify-between'>
+  <div class='flex justify-end w-full'>
+    <button @click=toggleMenu v-show=showMenuIcon class="md:hidden pl-3  text-white focus:outline-none h-20 w-20 justify-center items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-12 w-12 "
@@ -100,20 +101,29 @@ onMounted(()=>{
         </svg>
 
 
-        </button>
-        <button @click=toggleMenu v-show= mobileNav class="md:hidden flex absolute right-0   text-white focus:outline-none h-20 w-20 justify-center items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 6L18 18M18 6L6 18" />
-          </svg>
-        </button>
-      </div>
-      <div v-show= mobileNav class='lg:hidden flex fixed flex-col bg-barber-bg h-screen w-screen text-3xl  text-white font-hilborn tracking-wider pt-40 items-center'>
+  </button>
+  </div>
+    
+  
+  
+</div>
+        
+        
+      
+      <div v-show= mobileNav class='lg:hidden right-0 left-0 flex fixed flex-col bg-barber-bg h-screen w-screen text-3xl  text-white font-hilborn tracking-wider items-center'>
         <ul v-show= mobileNav >
             <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
-            <li class='py-10'><a href=# >PRECIOS</a></li>
-            <li class='py-10'><a href=# >AGENDAR</a></li>
-            <li class='py-10'><a href=# >UBICACIÓN</a></li>
-            <li class='py-10'><a href=# >CLIENTES</a></li>
+            <li class='pb-20'>             
+              <button @click=toggleMenu v-show= mobileNav class="md:hidden  flex absolute right-4  text-white focus:outline-none h-20 w-20 justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 6L18 18M18 6L6 18" />
+                </svg>
+              </button>
+            </li>
+            <li class='py-10 text-center'><a href=# >PRECIOS</a></li>
+            <li class='py-10 text-center'><a href=# >AGENDAR</a></li>
+            <li class='py-10 text-center'><a href=# >UBICACIÓN</a></li>
+            <li class='py-10 text-center'><a href=# >CLIENTES</a></li>
             <!-- <li class='sm:hidden md:hidden'><img :src=moustacheSvgLogo alt="" width="60"></li> -->
         </ul>
       </div>
