630f1bab377178b715c7ad2b2da2fe1b5165cea0
Xavier Pilasi <pilasiha@gmail.com>
2024-05-04 00:10:26 +0200

    cleaning the code

--- src/components/MenuTop.vue
+++ src/components/MenuTop.vue
@@ -12,6 +12,7 @@ const mobileNav = ref(null);
 const showMenuIcon =ref(true);
 const windowWidth = ref(null);
 
+
 const toggleMenu = ()=> {
   mobileNav.value = !mobileNav.value;
   showMenuIcon.value = !showMenuIcon.value;
@@ -23,21 +24,15 @@ const checkScreen = ()=>{
   
   if(windowWidth.value < 768){
     mobile.value = true; 
-    console.log('WTF - Mobile: '+mobile.value);
-    console.log('WTF - Mobile: '+mobile.value+' - ShowMenuIcon: '+showMenuIcon.value);
-    
+
   }else{
     mobileNav.value = false;
-  mobile.value = false;
-  showMenuIcon.value = true;
+    mobile.value = false;
+    showMenuIcon.value = true;
   
   }
-  
-  
-  
 
 }
-
 
 //menu scrolling down
 const handleScroll = ()=>{
@@ -51,7 +46,6 @@ onMounted(()=>{
     
     window.addEventListener('scroll',handleScroll);
     window.addEventListener('resize',checkScreen);
-    
     checkScreen();
     
 });
@@ -61,14 +55,9 @@ onMounted(()=>{
 <template>
    <header>
     <nav>
-
-    
-    
     <div>
-      
-      
+
       <!-- Nav no Mobile -->
-      
         <ul v-show= !mobile class='flex fixed right-0 left-0  bg-barber-bg  text-white font-hilborn tracking-wider justify-center items-center'>
             <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
             <li :class=menuClasses><a href=# >PRECIOS</a></li>
@@ -77,67 +66,51 @@ onMounted(()=>{
             <li :class=menuClasses><a href=# >CLIENTES</a></li>
             <!-- <li class='sm:hidden md:hidden'><img :src=moustacheSvgLogo alt="" width="60"></li> -->
         </ul>
-       
-      
-        
       <!-- Nav mobile -->
-      
-<div class='flex fixed w-full left-0 right-0 bg-barber-bg justify-between'>
-  <div class='flex justify-end w-full'>
-    <button @click=toggleMenu v-show=showMenuIcon class="md:hidden pl-3  text-white focus:outline-none h-20 w-20 justify-center items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-12 w-12 "
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg>
+      <div class='flex fixed w-full left-0 right-0 bg-barber-bg justify-between'>
+        <div class='flex justify-end w-full'>
+          <button @click=toggleMenu v-show=showMenuIcon class="md:hidden pl-3  text-white focus:outline-none h-20 w-20 justify-center items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-12 w-12 "
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
 
 
-  </button>
-  </div>
-    
-  
-  
-</div>
+        </button>
+        </div>
         
-        
-      
-      <div v-show= mobileNav class='lg:hidden right-0 left-0 flex fixed flex-col bg-barber-bg h-screen w-screen text-3xl  text-white font-hilborn tracking-wider items-center'>
-        <ul v-show= mobileNav >
-            <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
-            <li class='pb-20'>             
-              <button @click=toggleMenu v-show= mobileNav class="md:hidden  flex absolute right-4  text-white focus:outline-none h-20 w-20 justify-center items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 6L18 18M18 6L6 18" />
-                </svg>
-              </button>
-            </li>
-            <li class='py-10 text-center'><a href=# >PRECIOS</a></li>
-            <li class='py-10 text-center'><a href=# >AGENDAR</a></li>
-            <li class='py-10 text-center'><a href=# >UBICACIÓN</a></li>
-            <li class='py-10 text-center'><a href=# >CLIENTES</a></li>
-            <!-- <li class='sm:hidden md:hidden'><img :src=moustacheSvgLogo alt="" width="60"></li> -->
-        </ul>
-      </div>
+      </div>  
+        <div v-show= mobileNav class='lg:hidden right-0 left-0 flex fixed flex-col bg-barber-bg h-screen w-screen text-3xl  text-white font-hilborn tracking-wider items-center'>
+          <ul v-show= mobileNav >
+              <!-- <li class='sm:hidden md:hidden lg:visible '><img :src=moustacheSvgLogo alt="" width="60"></li> -->
+              <li class='pb-20'>             
+                <button @click=toggleMenu v-show= mobileNav class="md:hidden  flex absolute right-4  text-white focus:outline-none h-20 w-20 justify-center items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 6L18 18M18 6L6 18" />
+                  </svg>
+                </button>
+              </li>
+              <li class='py-10 text-center'><a href=# >PRECIOS</a></li>
+              <li class='py-10 text-center'><a href=# >AGENDAR</a></li>
+              <li class='py-10 text-center'><a href=# >UBICACIÓN</a></li>
+              <li class='py-10 text-center'><a href=# >CLIENTES</a></li>
+              <!-- <li class='sm:hidden md:hidden'><img :src=moustacheSvgLogo alt="" width="60"></li> -->
+          </ul>
+        </div>
         
     </div>
    
     </nav>
-
-
-    
-
-    
-
 </header>
-
 </template>
 
