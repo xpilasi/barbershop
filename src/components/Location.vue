74cb508dd5565a44b1d5bf7458b8e6933c3a54bc
Xavier Pilasi <pilasiha@gmail.com>
2024-05-28 17:40:20 +0200

    Google maps added

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,19 +1,22 @@
 
-<script setup>
+    <script setup>
+    import {ref,onMounted} from 'vue';
 
+    const map = ref(null);
 
+    onMounted(()=>{
+        
+        new window.google.maps.Map(map.value,{
+            center: {lat:41.40703 ,lng:2.16395},
+            zoom: 10,
+        });
+        
+    });
 
-</script>
-<template>
-<div class=' bg-gray-800 pt-10 pb-10 flex justify-center items-center h-96'>
-    <div>
-        <a href="#" class='bg-red-400 p-5'>Location</a>
-    </div>
-</div>
-
-    
-
-</input>
-
-</template>
+    </script>
+    <template>
+        
+        <div ref="map" style="width: 1000px; height: 500px;">hola</div>
+        
+    </template>
 
