1334f633934e2177eb7c2e91de5f145469b47133
Xavier Pilasi <pilasiha@gmail.com>
2024-07-03 23:59:24 +0200

    precios fixed

--- src/components/Prices.vue
+++ src/components/Prices.vue
@@ -18,37 +18,37 @@ import BookingButton from './BookingButton.vue';
                 <Title class='flex justify-center  mx-20 pt-40 pb-20'/>
                 <article class='grid lg:grid-cols-2 md:grid-cols-2 lg:gap-20   md:gap-20 font-futuraLight tracking-wider mx-20 lg:mx-56  lg:text-lg  '>
                     
-                <div class=" border-b-2 border-neutral-600 pb-2 mr-1">
+                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 md:pt-0 lg:pt-0 sm:pt-5 pt-5">
                     <div class="grid grid-cols-2 pb-0">
                         <p class="text-start">Corte Chico</p>
                         <p class=" text-end  ">EUR 12.80</p>
                     </div>
                 </div>
-                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0" >
+                <div class=" border-b-2 border-neutral-600 pb-2 mr-1  md:pt-0 lg:pt-0 sm:pt-5 pt-5 " >
                     <div class="grid grid-cols-2 pb-0">
-                        <p class="text-start">Corte Chico</p>
+                        <p class="text-start">Barba</p>
                         <p class="text-end  ">EUR 12.80</p>
                     </div>
                 </div>
-                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0 ">
+                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0 pt-5  ">
                     <div class="grid grid-cols-2 pb-0">
                         <p class="text-start">Corte Chico</p>
                         <p class="text-end  ">EUR 12.80</p> 
                     </div>
                 </div>
-                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0">
+                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0 pt-5 ">
                     <div class="grid grid-cols-2 pb-0">
                         <p class="text-start">Corte Chico</p>
                         <p class="text-end  ">EUR 12.80</p>
                     </div>
                 </div>
-                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0 ">
+                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0 pt-5 ">
                     <div class="grid grid-cols-2 pb-0">
                         <p class="text-start">Corte Chico</p>
                         <p class="text-end  ">EUR 12.80</p>
                     </div>
                 </div>
-                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0">
+                <div class=" border-b-2 border-neutral-600 pb-2 mr-1 sm:pt-5 md:pt-0 lg:pt-0 pt-5 ">
                     <div class="grid grid-cols-2 pb-0">
                         <p class="text-start">Corte Chico</p>
                         <p class="text-end  ">EUR 12.80</p>
