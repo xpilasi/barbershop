d9454f90548e0214595859c8c9ab1628db4b5318
Xavier Pilasi <pilasiha@gmail.com>
2024-07-06 14:13:52 +0200

    token ig added

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'
+import InstagramFeed from "vue3-instagram-feed";
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(App).use(InstagramFeed).mount("#app");
