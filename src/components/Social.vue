cb88541b6c09af07adb3db96667b9b06dbe2da38
Xavier Pilasi <pilasiha@gmail.com>
2024-07-15 22:12:09 +0200

    simple gallery

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -2,18 +2,22 @@
 import {ref,onMounted} from 'vue';
 import Title from  './Titles/Title.vue';
 import Galeria from './widgets/Gallery.vue';
+import GaleriaGrid from './widgets/GalleryGrid.vue';
 
 
 
 
 </script>
 <template>
-    <section id="galeria" class=" bg-barber-gallery min-h-screen bg-cover text-barber-bg pt-28">
-      <div class="justify-center flex">
+    <section id="galeria" class=" bg-barber-gallery min-h-screen bg-cover text-barber-bg flex items-center ">
+      <!-- <div class="justify-center flex">
                 <Title title="Instagram"/>
-        </div> 
-        <div class="flex justify-center  py-10">
+        </div>  -->
+        <!-- <div class="flex justify-center  py-10">
             <Galeria/>
+        </div> -->
+        <div class="">
+            <GaleriaGrid/>
         </div>
         
     </section>
---
+++ src/components/widgets/GalleryGrid.vue
@@ -0,0 +1,25 @@
+<script setup>
+import fotoGaleria1 from '../../assets/img/social/pictures/closeup-profile-bearded-man-with-tattoo-his-neck-sunglasses-wearing-military-shirt-studio-photo-against-dark-wall.jpg';
+import fotoGaleria2 from '../../assets/img/social/pictures/hairdressing-chair-vintage-barber-shop.jpg';
+import fotoGaleria3 from '../../assets/img/social/pictures/man-getting-his-hair-cut-barber-shop4.jpg';
+import fotoGaleria4 from '../../assets/img/social/pictures/portrait-two-male-barbers-looking-camera.jpg';
+import fotoGaleria5 from '../../assets/img/social/pictures/stylish-man-sitting-barbershop.jpg';
+import fotoGaleria6 from '../../assets/img/social/pictures/woman-washing-hair-customer.jpg';
+import fotoGaleria7 from '../../assets/img/social/pictures/woman-wrapping-face-client-barbershop.jpg';
+import fotoGaleria8 from '../../assets/img/social/pictures/47695.jpg';
+
+</script>
+
+<template>
+
+    <article>
+        <div class="grid md:grid-cols-3">
+            <img :src=fotoGaleria7 class="grid-item" alt="">
+            <img :src=fotoGaleria2 class="grid-item" alt="">
+            <img :src=fotoGaleria3 class="grid-item" alt="">
+            <img :src=fotoGaleria4 class="grid-item" alt="">
+            <img :src=fotoGaleria5 class="grid-item" alt="">
+            <img :src=fotoGaleria8 class="grid-item" alt="">
+        </div>
+    </article>
+</template>
