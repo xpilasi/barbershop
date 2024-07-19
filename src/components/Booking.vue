9d664b3e2f2673332bf8200f387626fbb302e5aa
Xavier Pilasi <pilasiha@gmail.com>
2024-07-19 19:21:30 +0200

    logo update

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -12,7 +12,7 @@ import Information from './widgets/Information.vue';
     <div class=' w-auto min-h-screen lg:px-36 lg:pt-28 flex flex-col  sm:px-5 md:px-20 lg:px-50  py-20 justify-between'>
   
         
-        <img class=" flex px-10 pt-20 lg:px-36 lg:pt-20  w-full  " :src=BarberShopLogo alt="" width="500">
+        <img class=" flex px-10 lg:px-72 py-10 w-full  justify-center items-center" :src=BarberShopLogo alt="">
         
         <div id="booking_booking button" class='flex justify-center  items-center h-40 '><BookingButton/></div>
         <div class="">
